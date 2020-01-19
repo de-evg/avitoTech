e5e8b14a6531494523219ce9167041539e9baf4e
de-evg <d.e.min@yandex.ru>
2020-01-19 16:06:39 +0300

    add css

--- src/components/realty.js
+++ src/components/realty.js
@@ -1,10 +1,10 @@
 const createRealtyCard = () => {
   return (
-    `<li class="realtyCard realty">
-       <h2 class="realty__title"></h2>
-       <p class="realty__price"></p>
-       <p class="realty__address"></p>
-       <img class="realty__preview" src="#" alt="" width="640" height="480">
+    `<li class="realtyCard">
+       <h2 class="realtyCard__title disableStyle"><a class="realtyCard__LinkAboutRealty" href="">Заголовок карточки</a></h2>
+       <p class="realtyCard__price disableStyle">Цена: 10000 руб.</p>
+       <p class="realtyCard__address disableStyle">Адрес: Москва, пр-кт Вернадского, д.10к2, кв.168</p>
+       <img class="realtyCard__preview realtyCard__preview--bgc" src="#" alt="" width="236" height="181">
     </li>`
   );
 };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import {createRealtyCard} from './components/realty.js';
 
-const renderCard = (container, template) => {
-  container.insertAdjacentHTML(`beforeEnd`, template);
+const NUMBER_OF_CARDS = 10;
+const renderCards = (container, template, count) => {
+  for (let i = 0; i < count; i++) {
+    container.insertAdjacentHTML(`beforeEnd`, template);
+  }
 };
 
 const mainElement = document.querySelector(`.page-main`);
 const realtyList = mainElement.querySelector(`.realtyList`);
 
-renderCard(realtyList, createRealtyCard());
+
+renderCards(realtyList, createRealtyCard(), NUMBER_OF_CARDS);
