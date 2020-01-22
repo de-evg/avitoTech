1bb633bcfa91768b4801bdb63d586aaf0fc2a274
de-evg <d.e.min@yandex.ru>
2020-01-22 21:05:18 +0300

    update css for realty cards

--- src/components/realty.js
+++ src/components/realty.js
@@ -1,12 +1,17 @@
 const createRealtyCard = (realty) => {
   return (
     `<li class="realtyCard" id="${realty.id}">
-       <h2 class="realtyCard__title disableStyle"><a class="realtyCard__LinkAboutRealty" href="card.html" ">${realty.title}</a></h2>
+       <h2 class="realtyCard__title disableStyle">
+         <a class="realtyCard__LinkAboutRealty" href="card.html" ">${realty.title}</a>
+       </h2>
+       <a class="realtyCard__link" href="card.html">
+         <img class="realtyCard__preview realtyCard__preview--bgc realtyCard__LinkAboutRealty" src="${realty.previewImage}" alt="" width="200" height="160">
+       </a>
        <p class="realtyCard__price disableStyle">Цена: ${realty.price}</p>
        <p class="realtyCard__address disableStyle">Адрес: ${realty.address}</p>
-       <a class="realtyCard__LinkAboutRealty" href="card.html">
-         <img class="realtyCard__preview realtyCard__preview--bgc realtyCard__LinkAboutRealty" src="${realty.previewImage}" alt="" width="236" height="181">
-       </a>
+       <span>
+         <a class="realtyCard__btn realtyCard__LinkAboutRealty" href="card.html">Подробнее</a>
+       </span>
     </li>`
   );
 };
