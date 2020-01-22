3aab46309644933067b1b89126c765c2d513431d
de-evg <d.e.min@yandex.ru>
2020-01-22 16:54:54 +0300

    update css for slider and index.html

--- src/components/aboutRealty.js
+++ src/components/aboutRealty.js
@@ -1,9 +1,9 @@
 const createRealtyInfo = (realty) => {
-  const generateGalleryTemplates = (images) => {
+  const generateGalleryTemplates = (realtyData) => {
     let galleryListItems = [];
-    images.forEach((image) => {
+    realtyData.images.forEach((image) => {
       galleryListItems.push(`<li class="realty-gallery__item slide">
-        <img class="realty-gallery__image realtyCard__preview--bgc" src="${image}" alt="">
+        <img class="realty-gallery__image realtyCard__preview--bgc" src="${image}" alt="Фото ${realtyData.title}">
        </li>`);
     });
     return galleryListItems.join(`\n`);
@@ -11,15 +11,15 @@ const createRealtyInfo = (realty) => {
 
   return (
     `<h2 class="realty__title disableStyle">${realty.title}</h2>
-     <p class="realty__price disableStyle">Цена: ${realty.price}</p>
-     <p class="realty__address disableStyle">Адрес: ${realty.address}</p>
-     <p class="realty__description disableStyle">Описание: ${realty.description}</p>
-     <p class="realty__seller disableStyle">Продавец: ${realty.sellerName}</p>
+     <p class="realty__price disableStyle"><span>Цена:</span> ${realty.price}</p>
+     <p class="realty__address disableStyle"><span>Адрес:</span> ${realty.address}</p>
+     <p class="realty__description disableStyle"><span>Описание:</span> ${realty.description}</p>
+     <p class="realty__seller disableStyle"><span>Продавец:</span> ${realty.sellerName}</p>
      <section class="realty-gallery slider">
        <button class="realty-gallery__prevBtn slider__prevBtn"></button>
        <button class="realty-gallery__nextBtn slider__nextBtn"></button>
        <ul class="realty-gallery__container disableStyle slider__container">
-        ${generateGalleryTemplates(realty.images)}
+        ${generateGalleryTemplates(realty)}
        </ul>
      </section>`
   );
