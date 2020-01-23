a9bfd9d625b9c1d502f61719369fedde225da856
de-evg <d.e.min@yandex.ru>
2020-01-23 20:31:13 +0300

    add regExp to fix text

--- src/utils.js
+++ src/utils.js
@@ -3,9 +3,12 @@ const checkTypo = (data) => {
   keys.forEach((key) => {
     let newString = data[key];
     if (typeof newString === `string`) {
-      const regExp = [/,,/, /руб./gi];
+      const regExp = [/,,/, /руб./gi, /ж,/, /,[A-Za-zА-Яа-я0-9_]/];
       newString = newString.replace(regExp[0], `,`);
       newString = newString.replace(regExp[1], `&#8381`);
+      newString = newString.replace(regExp[2], `ж`);
+      newString = newString.split(`,`).join(`, `);
+      newString = newString.slice(0, 1).toUpperCase() + newString.slice(1);
     }
     data[key] = newString;
   });
